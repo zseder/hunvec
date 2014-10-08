ecccf0026ed3a6c93194961ede7b17369a412ab0
zseder <zseder@ilab.sztaki.hu>
2014-10-08 18:39:15 +0200

    modified:   nnlm.py - more complex stop criteria - saving best model to file

--- nnlm.py
+++ nnlm.py
@@ -4,17 +4,21 @@ from pylearn2.space import IndexSpace
 from pylearn2.models.mlp import MLP, Tanh
 from pylearn2.sandbox.nlp.models.mlp import ProjectionLayer, Softmax
 from pylearn2.training_algorithms.sgd import SGD
-from pylearn2.termination_criteria import MonitorBased
+from pylearn2.termination_criteria import MonitorBased, And, EpochCounter
 from pylearn2.train import Train
+from pylearn2.train_extensions.best_params import MonitorBasedSaveBest
 
 from corpus import Corpus
 
 
 class NNLM(object):
-    def __init__(self, hidden_dim=20, window_size=3, embedding_dim=10):
+    def __init__(self, hidden_dim=20, window_size=3, embedding_dim=10,
+                 optimize_for='valid_softmax_nll', max_epochs=10000):
         self.hdim = hidden_dim
         self.window_size = window_size
         self.edim = embedding_dim
+        self.optimize_for = optimize_for
+        self.max_epochs = max_epochs
 
     def add_corpus(self, corpus):
         self.vocab_size = len(corpus.vocab)
@@ -39,22 +43,26 @@ class NNLM(object):
         self.model = model
 
     def create_algorithm(self):
-        term = MonitorBased(channel_name='valid_softmax_nll', prop_decrease=0.,
-                            N=10)
+        cost_crit = MonitorBased(channel_name=self.optimize_for,
+                                 prop_decrease=0., N=10)
+        epoch_cnt_crit = EpochCounter(max_epochs=self.max_epochs)
+        term = And(criteria=[cost_crit, epoch_cnt_crit])
         self.algorithm = SGD(batch_size=100, learning_rate=.01,
                              monitoring_dataset=self.alg_datasets,
                              termination_criterion=term)
 
-    def create_training_problem(self):
+    def create_training_problem(self, save_best_path):
+        ext1 = MonitorBasedSaveBest(channel_name=self.optimize_for,
+                                    save_path=save_best_path)
         trainer = Train(dataset=self.alg_datasets['train'], model=self.model,
-                        algorithm=self.algorithm, extensions=[])
+                        algorithm=self.algorithm, extensions=[ext1])
         self.trainer = trainer
 
 
 def main():
     nnlm = NNLM()
     corpus = Corpus.read_corpus(sys.argv[1])
-    corpus.filter_freq(1000)
+    corpus.filter_freq(10000)
     nnlm.add_corpus(corpus)
     nnlm.create_model()
     nnlm.create_algorithm()
