b25d6f0267e6111c68376bcfe3f8be5949673d5f
zseder <zseder@ilab.sztaki.hu>
2014-10-14 16:47:18 +0200

    modified:   predict_nnlm.py little advanced predicting

--- predict_nnlm.py
+++ predict_nnlm.py
@@ -5,8 +5,14 @@ import numpy
 
 from pylearn2.utils import serial
 
-inputs = numpy.array([[0, 1, 2]])
+inputs = numpy.array([[0, 1, 2], [1, 2, 3], [2, 3, 4], [111, 26, 41]])
 
 model_path = sys.argv[1]
 model = serial.load(model_path)
-print model.fprop(theano.shared(inputs, name='inputs')).eval()
+results = model.fprop(theano.shared(inputs, name='inputs')).eval()
+for i in xrange(len(results)):
+    input_ = inputs[i, :]
+    res = results[i]
+    print input_
+    sorted_indices = res.argsort()[-5:]
+    print sorted_indices
