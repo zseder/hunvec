a643c1f72064ca9ec8c3c5628804d5e9da2c132d
zseder <zseder@ilab.sztaki.hu>
2014-10-15 13:38:38 +0200

    modified:   predict_nnlm.py betteer predicting by using all the test examples

--- predict_nnlm.py
+++ predict_nnlm.py
@@ -5,14 +5,52 @@ import numpy
 
 from pylearn2.utils import serial
 
-inputs = numpy.array([[0, 1, 2], [1, 2, 3], [2, 3, 4], [111, 26, 41]])
+from corpus import Corpus
 
-model_path = sys.argv[1]
-model = serial.load(model_path)
-results = model.fprop(theano.shared(inputs, name='inputs')).eval()
-for i in xrange(len(results)):
-    input_ = inputs[i, :]
-    res = results[i]
-    print input_
-    sorted_indices = res.argsort()[-5:]
-    print sorted_indices
+
+def manual_test():
+    inputs = numpy.array([[i, j] for i in xrange(500) for j in xrange(500)])
+
+    model_path = sys.argv[1]
+    model = serial.load(model_path)
+    results = model.fprop(theano.shared(inputs, name='inputs')).eval()
+    for i in xrange(len(results)):
+        input_ = inputs[i, :]
+        res = results[i]
+        #print input_
+        sorted_indices = res.argsort()[-5:]
+        if sorted_indices.tolist() != [11, 23, 8, 10, 3]:
+            print "{0}\t{1}".format(input_, sorted_indices)
+        #print sorted_indices
+
+
+def corpus_test(model, corpus):
+    tr, v, test = corpus.get_matrices(3)
+    good, bad = 0, 0
+    targets = test.get_targets()
+    b = 1000
+    c = 0
+    for inputs in test.iterator(mode='sequential', batch_size=b):
+        outputs = targets[c*1000:(c+1)*1000]
+
+        results = model.fprop(theano.shared(inputs, name='inputs')).eval()
+        for i in xrange(len(results)):
+            res = results[i]
+            sorted_best_5 = res.argsort()[-5:]
+            gold = outputs[i]
+            if gold in sorted_best_5:
+                good += 1
+            else:
+                bad += 1
+        print "Batch done", good, bad
+    print good, bad
+
+
+def main():
+    m = serial.load(sys.argv[1])
+    c = Corpus.read_corpus(sys.argv[2])
+    c.filter_freq(n=5000)
+    corpus_test(m, c)
+
+if __name__ == "__main__":
+    main()
