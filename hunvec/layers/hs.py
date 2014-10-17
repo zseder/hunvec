aee402369c3704b931d7ebab3423d618bf6ccf5d
zseder <zseder@ilab.sztaki.hu>
2014-10-17 17:35:44 +0200

    modified:   hunvec/layers/hs.py modified:   setup.py hs setup'd hs used

--- hunvec/layers/hs.py
+++ hunvec/layers/hs.py
@@ -2,8 +2,12 @@ from pylearn2.models.mlp import Sigmoid
 
 
 class HierarchicalSoftmax(Sigmoid):
-    def __init__():
+    def __init__(self, vocab_size, **kwargs):
+        # TODO set dim based on vocab size
+        super(HierarchicalSoftmax, self).__init__(**kwargs)
         pass
 
-    def cost(self, Y, Y_hat):
+    def cost_XXX(self, Y, Y_hat):
+        # TODO iterate through Y, where Y==1, compute kl and multiply them
+        # at the end
         pass
