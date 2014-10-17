3583ef010670640a00f29d655075f463de59dc53
zseder <zseder@ilab.sztaki.hu>
2014-10-17 17:37:21 +0200

    modified:   hunvec/layers/hs.py dimension 'computed'

--- hunvec/layers/hs.py
+++ hunvec/layers/hs.py
@@ -4,8 +4,8 @@ from pylearn2.models.mlp import Sigmoid
 class HierarchicalSoftmax(Sigmoid):
     def __init__(self, vocab_size, **kwargs):
         # TODO set dim based on vocab size
-        super(HierarchicalSoftmax, self).__init__(**kwargs)
-        pass
+        dim = vocab_size
+        super(HierarchicalSoftmax, self).__init__(dim=dim, **kwargs)
 
     def cost_XXX(self, Y, Y_hat):
         # TODO iterate through Y, where Y==1, compute kl and multiply them
