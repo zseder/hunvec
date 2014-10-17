8deddfe814c6b8a7a5265043faadf076f6445001
zseder <zseder@ilab.sztaki.hu>
2014-10-17 17:22:30 +0200

    modified:   hunvec/utils/binary_tree.py encode word into vector representation of its binary tree

--- hunvec/utils/binary_tree.py
+++ hunvec/utils/binary_tree.py
@@ -2,7 +2,6 @@
 Copied from http://rosettacode.org/wiki/Huffman_coding#Python and modified.
 """
 from heapq import heappush, heappop, heapify
-
 import numpy
 
 
@@ -18,12 +17,25 @@ def create_binary_tree(symb2freq):
         for pair in hi[1:]:
             pair[1] = '1' + pair[1]
         heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
-    words = sorted(heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
-    btree_len = max(len(b) for _, b in words)
-    d = {}
-    for w, b in words:
-        d[w] = numpy.zeros(btree_len, dtype='int8')
-        for ch_i in xrange(len(b)):
-            if b[ch_i] == '1':
-                d[w][ch_i] = 1
-    return d
+    return sorted(heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+
+
+def vector_encoder(t):
+    prefixes = reduce(lambda x, y: x | y,
+                      [set(code[:i] for i in
+                           xrange(len(code))) for w, code in t])
+    alpha_sorted = sorted(prefixes)
+    len_sorted = sorted(alpha_sorted, key=lambda x: len(x))
+    prefixes = dict((prefix, i) for i, prefix in enumerate(len_sorted))
+    encoder = {}
+    for w, code in t:
+        new_code = numpy.ones(len(prefixes))
+        new_code *= -1
+        for pr_i in xrange(len(code)):
+            pr = code[:pr_i]
+            if code[pr_i] == '0':
+                new_code[prefixes[pr]] = 0
+            else:
+                new_code[prefixes[pr]] = 1
+        encoder[w] = new_code
+    return encoder
