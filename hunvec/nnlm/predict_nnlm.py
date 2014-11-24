fb090f9c8a3baf70e2accf956377039831c100a4
zseder <zseder@ilab.sztaki.hu>
2014-11-24 15:13:02 +0100

    modified:   hunvec/nnlm/predict_nnlm.py lot of fixes to follow recent changes

--- hunvec/nnlm/predict_nnlm.py
+++ hunvec/nnlm/predict_nnlm.py
@@ -5,7 +5,7 @@ import numpy
 
 from pylearn2.utils import serial
 
-from corpus import Corpus
+from hunvec.corpus.corpus import Corpus
 
 
 def manual_test():
@@ -25,31 +25,47 @@ def manual_test():
 
 
 def corpus_test(model, corpus):
-    tr, v, test = corpus.get_matrices(3)
     good, bad = 0, 0
-    targets = test.get_targets()
-    b = 1000
     c = 0
-    for inputs in test.iterator(mode='sequential', batch_size=b):
-        outputs = targets[c*1000:(c+1)*1000]
+    X = model.get_input_space().make_theano_batch()
+    Y = model.fprop(X)
+    f = theano.function([X], Y)
+    v = set(corpus.vocab.itervalues())
+    v = [corpus.w_enc(w) for w in v]
+    while True:
+        dataset = corpus.create_batch_matrices()
+        if dataset is None:
+            break
+        test = dataset[2]
+        Y = f(test.X)
 
-        results = model.fprop(theano.shared(inputs, name='inputs')).eval()
-        for i in xrange(len(results)):
-            res = results[i]
-            sorted_best_5 = res.argsort()[-5:]
-            gold = outputs[i]
-            if gold in sorted_best_5:
+        targets = test.get_targets()
+        outputs = targets[c*1000:(c+1)*1000]
+        for i in xrange(outputs.shape[0]):
+            tgt = outputs[i]
+            needed = tgt >= 0
+            y = Y[c*1000 + i]
+            #distances = numpy.array([cosine(y[needed], w[needed]) for w in v])
+            probs = numpy.array([y[w == 0].prod() * (1.0 - y[w == 1]).prod()
+                                 for w in v])
+            closests = set([tuple(v[w][needed])
+                            for w in probs.argsort()[-100:]])
+            if tuple(tgt[needed]) in closests:
                 good += 1
             else:
                 bad += 1
+            print good, bad
+
+        c += 1
+
         print "Batch done", good, bad
     print good, bad
 
 
 def main():
     m = serial.load(sys.argv[1])
-    c = Corpus.read_corpus(sys.argv[2])
-    c.filter_freq(n=5000)
+    c = Corpus(sys.argv[2], batch_size=1000, window_size=5, hs=True,
+               max_corpus_epoch=1)
     corpus_test(m, c)
 
 if __name__ == "__main__":
