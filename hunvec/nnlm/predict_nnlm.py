e66e8fec65f3db53ca5e85fa4bd1bf09c38d753d
zseder <zseder@ilab.sztaki.hu>
2014-11-27 17:55:30 +0100

    modified:   hunvec/nnlm/predict_nnlm.py     very hacky changes to support hs and non-hs prediction, lot of     printouts, all that shit

--- hunvec/nnlm/predict_nnlm.py
+++ hunvec/nnlm/predict_nnlm.py
@@ -24,19 +24,25 @@ def manual_test():
         #print sorted_indices
 
 
-def corpus_test(model, corpus):
+def corpus_test(model, corpus, hs):
     good, bad = 0, 0
     c = 0
     X = model.get_input_space().make_theano_batch()
     Y = model.fprop(X)
     f = theano.function([X], Y)
-    v = set(corpus.vocab.itervalues())
-    v = [corpus.w_enc(w) for w in v]
+    if hs:
+        v = set(corpus.vocab.itervalues())
+        v = [corpus.w_enc(w) for w in v]
+        d = {}
+        for w, i in corpus.vocab.iteritems():
+            code = corpus.w_enc(i)
+            d[tuple(code)] = w
+
     while True:
         dataset = corpus.create_batch_matrices()
         if dataset is None:
             break
-        test = dataset[2]
+        test = dataset[0]
         Y = f(test.X)
 
         targets = test.get_targets()
@@ -45,15 +51,25 @@ def corpus_test(model, corpus):
             tgt = outputs[i]
             needed = tgt >= 0
             y = Y[c*1000 + i]
-            #distances = numpy.array([cosine(y[needed], w[needed]) for w in v])
-            probs = numpy.array([y[w == 0].prod() * (1.0 - y[w == 1]).prod()
-                                 for w in v])
-            closests = set([tuple(v[w][needed])
-                            for w in probs.argsort()[-5:]])
-            if tuple(tgt[needed]) in closests:
-                good += 1
+            if i == 100:
+                quit()
+            if hs:
+                probs = numpy.array([numpy.log(y[w == 0]).sum() + numpy.log(1.0 - y[w == 1]).sum()
+                                     for w in v])
+                closests = set([tuple(v[w][needed])
+                                for w in probs.argsort()[-5:]])
+                for w in probs.argsort()[-5:]:
+                    ww = tuple(v[w])
+                    print d[ww]
+                if tuple(tgt[needed]) in closests:
+                    good += 1
+                else:
+                    bad += 1
             else:
-                bad += 1
+                closests = y.argsort()[-5:]
+                print tgt
+                print closests
+
             print good, bad
 
         c += 1
@@ -64,9 +80,9 @@ def corpus_test(model, corpus):
 
 def main():
     m = serial.load(sys.argv[1])
-    c = Corpus(sys.argv[2], batch_size=1000, window_size=5, hs=True,
-               max_corpus_epoch=1)
-    corpus_test(m, c)
+    c = Corpus(sys.argv[2], batch_size=1000, window_size=2, hs=False,
+               max_corpus_epoch=1, top_n=10000)
+    corpus_test(m, c, hs=False)
 
 if __name__ == "__main__":
     main()
