d09c17a034ba0cac6b9bc16a82d2a1a4579715b7
zseder <zseder@ilab.sztaki.hu>
2015-01-09 16:25:13 +0100

    modified:   hunvec/seqtag/word_tagger_dataset.py big ass changes

--- hunvec/seqtag/word_tagger_dataset.py
+++ hunvec/seqtag/word_tagger_dataset.py
@@ -7,37 +7,35 @@ from pylearn2.space import CompositeSpace, IndexSequenceSpace
 from pylearn2.space import VectorSequenceSpace
 from pylearn2.utils.iteration import FiniteDatasetIterator
 from pylearn2.utils.iteration import resolve_iterator_class
-from pylearn2.utils.iteration import SequentialSubsetIterator
 
-from hunvec.utils.data_splitter import datasplit
+from hunvec.utils.data_splitter import datasplit, shuffled_indices
 
 
-def create_splitted_datasets(wa, fa, ya, ratios):
-        vocab_size = wa.max() - wa.min() + 1
-        feat_num = fa.max() - fa.min() + 1
-        n_classes = ya.max() - ya.min() + 1
-        wa_train, wa_test, wa_valid = datasplit(wa, ratios)
-        fa_train, fa_test, fa_valid = datasplit(fa, ratios)
-        ya_train, ya_test, ya_valid = datasplit(ya, ratios)
-        kwargs = {
-            "vocab_size": vocab_size,
-            "window_size": wa_train.shape[1],
-            "feat_num": feat_num,
-            "n_classes": n_classes,
-        }
-        return {
-            'train': WordTaggerDataset((wa_train, fa_train), ya_train,
-                                       **kwargs),
-            'test': WordTaggerDataset((wa_test, fa_test), ya_test,
-                                      **kwargs),
-            'valid': WordTaggerDataset((wa_valid, fa_valid), ya_valid,
-                                       **kwargs)
-        }
+def create_splitted_datasets(wa, fa, ya, ratios,
+                             vocab_size, window_size, feat_num, n_classes):
+    indices = shuffled_indices(len(wa), ratios)
+    wa_train, wa_test, wa_valid = datasplit(wa, indices, ratios)
+    fa_train, fa_test, fa_valid = datasplit(fa, indices, ratios)
+    ya_train, ya_test, ya_valid = datasplit(ya, indices, ratios)
+    kwargs = {
+        "vocab_size": vocab_size,
+        "window_size": window_size,
+        "feat_num": feat_num,
+        "n_classes": n_classes,
+    }
+    d = {
+        'train': WordTaggerDataset((wa_train, fa_train), ya_train,
+                                   **kwargs),
+        'test': WordTaggerDataset((wa_test, fa_test), ya_test,
+                                  **kwargs),
+        'valid': WordTaggerDataset((wa_valid, fa_valid), ya_valid,
+                                   **kwargs)
+    }
+    return d
 
 
 class WordTaggerDataset(Dataset):
-    def __init__(self, X, y, vocab_size, window_size, feat_num, n_classes,
-                 iteration_mode=None):
+    def __init__(self, X, y, vocab_size, window_size, feat_num, n_classes):
         super(WordTaggerDataset, self).__init__()
         self.X1 = X[0]
         self.X2 = X[1]
@@ -47,16 +45,12 @@ class WordTaggerDataset(Dataset):
             IndexSequenceSpace(max_labels=feat_num, dim=window_size),
             VectorSequenceSpace(dim=n_classes)
         ))
-        source = ('inputs', 'features', 'targets')
+        source = ('words', 'features', 'targets')
         self.data_specs = (space, source)
         self.vocab_size = vocab_size
         self.window_size = window_size
         self.feat_num = feat_num
         self.n_classes = n_classes
-        if iteration_mode is None:
-            self.iteration_mode = SequentialSubsetIterator
-        else:
-            self.iteration_mode = iteration_mode
 
     def get_num_examples(self):
         return len(self.X1)
@@ -64,7 +58,11 @@ class WordTaggerDataset(Dataset):
     def get_data_specs(self):
         return self.data_specs
 
+    def get_monitoring_data_specs(self):
+        return self.data_specs
+
     def get_data(self):
+        #return itertools.izip(self.X1, self.X2, self.y)
         return self.X1, self.X2, self.y
 
     @functools.wraps(Dataset.iterator)
@@ -75,11 +73,15 @@ class WordTaggerDataset(Dataset):
             num_batches = len(self.X1) / (batch_size * 10)
 
         mode = resolve_iterator_class(mode)
+        if data_specs is None:
+            data_specs = self.data_specs
+        #convert = [lambda x: x[0]] * len(data_specs[1])
 
         i = FiniteDatasetIterator(
             self,
             mode(len(self.X1), batch_size, num_batches, rng),
-            data_specs=self.data_specs,
+            data_specs=data_specs, return_tuple=return_tuple,
+            #convert=convert
         )
         return i
 
@@ -89,25 +91,49 @@ class WordTaggerDataset(Dataset):
         words = []
         features = []
         y = []
-        pad = [(pad_num, pad_num)] * window_size
+        pad = [(pad_num, pad_num)] * (window_size - 1)
         # include the word itself
-        fake_feats = [0] * (window_size + 1)
+        fake_feats = [0] * (window_size)
+        vocab, classes, feats = set(), set(), set()
         for sen in c.corpus:
             sen = list(pad) + sen + list(pad)
+            sen_words, sen_features, sen_y = [], [], []
             # don't create data where y is pad
-            for word_i in xrange(window_size, len(sen) - window_size):
+            for word_i in xrange(window_size - 1, len(sen) - window_size + 1):
                 tag = sen[word_i][1]
 
                 # the word is there, too
-                window = [w for w, p in sen[word_i - window_size: word_i + 1]]
+                window = [w for w, p in
+                          sen[word_i - window_size + 1: word_i + 1]]
                 fs = fake_feats
-                words.append(window)
-                features.append(fs)
-                y.append([tag])
+                sen_words.append(window)
+                sen_features.append(fs)
+                sen_y.append([tag])
 
-        words_array = numpy.array(words)
-        feats_array = numpy.array(features)
-        y_array = numpy.array(y)
-        return create_splitted_datasets(words_array,
-                                        feats_array,
-                                        y_array, ratios)
+                # counting
+                vocab |= set(window)
+                feats |= set(fs)
+                classes.add(tag)
+
+            if len(sen_words) < 3:
+                continue
+            #if len(sen_words) > 20:
+            #    continue
+
+            words.append(numpy.array(sen_words))
+            features.append(numpy.array(sen_features))
+            y.append(sen_y)
+
+        y_ = []
+        for sen_y in y:
+            y_.append(numpy.zeros((len(sen_y), len(classes)),
+                                  dtype=numpy.float32))
+            for row_i, v in enumerate(sen_y):
+                y_[-1][row_i][v] = 1
+        y = y_
+
+        return create_splitted_datasets(words, features, y, ratios,
+                                        vocab_size=max(vocab),
+                                        window_size=window_size,
+                                        feat_num=len(feats),
+                                        n_classes=len(classes))
