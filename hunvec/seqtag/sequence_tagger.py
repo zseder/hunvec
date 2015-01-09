4cb0ec0e3edc5726dd8ccda64e3d23c01b631fa8
zseder <zseder@ilab.sztaki.hu>
2015-01-09 16:26:00 +0100

    modified:   hunvec/seqtag/sequence_tagger.py - huge amount of changes, now it converges to somewhere - needs a lot of documentation, testing, etc, probably does everything   wrong

--- hunvec/seqtag/sequence_tagger.py
+++ hunvec/seqtag/sequence_tagger.py
@@ -1,7 +1,11 @@
-import numpy
+import sys
 import functools
 
+import numpy
+
 import theano
+import theano.tensor as T
+from theano.tensor.nnet import softmax
 
 from pylearn2.models.model import Model
 from pylearn2.space import CompositeSpace, IndexSequenceSpace
@@ -11,23 +15,86 @@ from pylearn2.costs.cost import Cost, DefaultDataSpecsMixin
 from pylearn2.termination_criteria import EpochCounter
 from pylearn2.training_algorithms.sgd import SGD
 from pylearn2.train import Train
+from pylearn2.devtools.nan_guard import NanGuardMode
 
-from word_tagger import WordTaggerNetwork
-from word_tagger_dataset import WordTaggerDataset
+from hunvec.seqtag.word_tagger import WordTaggerNetwork
+from hunvec.seqtag.word_tagger_dataset import WordTaggerDataset
+from hunvec.corpus.tagged_corpus import TaggedCorpus
 
+
+        #if T.lt(tagger_out.shape[0], 2):
+        #    return start_mask
+
+        # tout: tagger output
+        # fn = lambda res_probs, tout: theano.dot(self.A, res_probs) * tout
+
+        #(probs_after_A, updates) = theano.scan(
+        #    fn=fn,
+        #    sequences=[tagger_out[1:]],
+        #    outputs_info=start_mask,
+        #)
 
 class SeqTaggerCost(DefaultDataSpecsMixin, Cost):
-    # Here it is assumed that we are doing supervised learning
     supervised = True
 
+    def combine_A_tout_scanner(self, prev_res, tagger_out, A):
+        # create a new matrix from A, add prev_res to every column
+        A_t_ = A.dimshuffle((1, 0))
+        A_t = A_t_ + prev_res
+
+        log_added = T.log(T.exp(A_t).sum(axis=1))
+
+        new_res = log_added + tagger_out
+        return softmax(new_res).flatten()
+
     def expr(self, model, data, **kwargs):
+        ## compute score as Collobert did
         space, source = self.get_data_specs(model)
         space.validate(data)
 
         inputs, targets = data
-        outputs = model.fprop(inputs)
-        # TODO word-wise softmax cost should come here
-        return theano.tensor.mean(theano.tensor.sqr(targets - outputs))
+        outputs = softmax(model.fprop(inputs))
+
+        # unpack A and tagger_out from outputs
+        start = outputs[0]
+        end = outputs[1]
+        A = outputs[2:2 + model.n_classes]
+        tagger_out = outputs[2 + model.n_classes:]
+
+        # compute normalizer factor NF for this given training data
+        start_M = tagger_out[0] + start
+        combined_probs, updates = theano.scan(
+            fn=self.combine_A_tout_scanner,
+            sequences=[tagger_out[1:]],
+            non_sequences=[A],
+            outputs_info=start_M
+        )
+        end_M = combined_probs[-1] + end
+        NF = T.log(T.exp(end_M).sum())
+
+        # compute gold seq's score with using A and tagger_out
+        gold_seq = targets.argmax(axis=1)
+        seq_score = start[gold_seq[0]] + end[gold_seq[-1]]
+
+        # tagger_out_scores
+        tout_chooser = lambda gold_index, i: tagger_out[i][gold_index]
+        tout_seq_scores, updates = theano.scan(
+            fn=tout_chooser,
+            sequences=[gold_seq, T.arange(gold_seq.shape[0])],
+            outputs_info=None
+        )
+        seq_score += tout_seq_scores.sum()
+
+        # A matrix scores
+        A_chooser = lambda i, next_i: A[i][next_i]
+        A_seq_scores, updates = theano.scan(
+            fn=A_chooser,
+            sequences=[gold_seq[:-1], gold_seq[1:]],
+            outputs_info=None
+        )
+        seq_score += A_seq_scores.sum()
+
+        return -(seq_score - NF)
 
 
 class SequenceTaggerNetwork(Model):
@@ -54,38 +121,15 @@ class SequenceTaggerNetwork(Model):
         self.tagger = WordTaggerNetwork(vocab_size, window_size, feat_num,
                                         hdim, edim, n_classes)
 
-        # start is included as 0th row to make T.scan easy
         A_value = numpy.random.uniform(low=-.1, high=.1,
-                                       size=(self.n_classes + 1,
+                                       size=(self.n_classes + 2,
                                              self.n_classes))
         self.A = sharedX(A_value, name='A')
-        end = numpy.random.uniform(low=-.1, high=.1, size=self.n_classes)
-        self.end = sharedX(end, name='start')
 
     def fprop(self, data):
         tagger_out = self.tagger.fprop(data)
-
-        # since we put starting probabilities into A[0,:], we only need
-        # a mask, so iteration is easier
-        start_mask = numpy.zeros(self.n_classes + 1, dtype=numpy.float32)
-        start_mask[0] = 1.0
-
-        # tout: tagger output
-        fn = lambda res_probs, tout: theano.dot(res_probs * self.A, tout)
-        (probs_after_A, updates) = theano.scan(
-            fn=fn,
-            sequences=[tagger_out],
-            outputs_info=start_mask
-        )
-
-        # use $ transitions
-        probs_after_A[-1].dot(self.end)
-
-        # run a theano.nnet.softmax on every row of probs_after_A
-        # TODO maybe a cost function would need this?
-        res = theano.tensor.nnet.softmax(probs_after_A)
-
-        return res
+        probs = T.concatenate([self.A, tagger_out])
+        return probs
 
     @functools.wraps(Model.get_params)
     def get_params(self):
@@ -94,8 +138,15 @@ class SequenceTaggerNetwork(Model):
     @functools.wraps(Model.get_monitoring_channels)
     def get_monitoring_channels(self, data):
         rval = Model.get_monitoring_channels(self, data)
-        # TODO add own channels
+        rval['A_min'] = self.A.min()
+        rval['A_max'] = self.A.max()
+        rval['h0_min'] = self.tagger.layers[1].get_params()[0].min()
+        rval['h0_max'] = self.tagger.layers[1].get_params()[0].max()
         return rval
+
+    #@functools.wraps(Model._modify_updates)
+    #def _modify_updates(self, updates):
+    #    updates[self.A] = softmax(self.A)
 
     def create_algorithm(self, data, save_best_path=None):
         self.dataset = data
@@ -103,9 +154,12 @@ class SequenceTaggerNetwork(Model):
         algorithm = SGD(batch_size=1, learning_rate=.1,
                         termination_criterion=epoch_cnt_crit,
                         monitoring_dataset=data,
+                        #monitoring_batch_size=1,
+                        #monitor_iteration_mode='sequential',
+                        theano_function_mode=NanGuardMode(nan_is_error=True, inf_is_error=True),
                         cost=SeqTaggerCost(),
                         )
-        self.trainer = Train(dataset=data, model=self,
+        self.trainer = Train(dataset=data['train'], model=self,
                              algorithm=algorithm)
 
 
@@ -118,9 +172,20 @@ def test_data():
         "edim": 10,
         "n_classes": 2
     }
-    X = [numpy.array([[0, 1, 2], [3, 5, 4]], dtype=numpy.int32),
-         numpy.array([[0, 1, 0], [1, 1, 1]], dtype=numpy.int32)]
-    y = numpy.array([[0], [0]], dtype=numpy.int32)
+
+    # two sentences, with 2 and 3 lengths
+    X = ([
+         [[0, 1, 2], [3, 5, 4]],
+         [[0, 1, 2], [3, 5, 4], [5, 4, 0]]
+         ],
+         [
+             [[0, 1, 0], [1, 1, 1]],
+             [[0, 1, 0], [1, 1, 1], [1, 0, 1]]
+         ])
+    y = [
+        [[0, 0]],
+        [[0, 1, 1]]
+    ]
     d = WordTaggerDataset(X, y,
                           vocab_size=params['vocab_size'],
                           window_size=params['window_size'],
@@ -136,5 +201,44 @@ def test():
     st.trainer.main_loop()
 
 
+def init_brown():
+    fn = sys.argv[1]
+    c = TaggedCorpus(fn)
+    d = WordTaggerDataset.create_from_tagged_corpus(c)
+    wt = SequenceTaggerNetwork(vocab_size=d['train'].vocab_size,
+                               window_size=d['train'].window_size,
+                               feat_num=d['train'].feat_num,
+                               n_classes=d['train'].n_classes,
+                               edim=10, hdim=20)
+    return c, d, wt
+
+
+def train_brown_pos():
+    c, d, wt = init_brown()
+    wt.create_algorithm(d, sys.argv[2])
+    wt.trainer.main_loop()
+
+
+def predict_test():
+    c, d, wt = init_brown()
+    #print d['train'].y[0]
+    #X = wt.get_input_space().make_theano_batch()
+    #Y = wt.fprop(X)
+    #f = theano.function([X[0], X[1]], Y)
+    #y = f(d['train'].X1[0], d['train'].X2[0])
+    #print y, y.shape
+    #print (d['train'].y[0] * y).sum()
+    #print len(d['train'].y[0])
+    cost = SeqTaggerCost()
+    words = T.matrix('words', dtype='int64')
+    features = T.matrix('features', dtype='int64')
+    targets = T.matrix('targets', dtype='float32')
+    cost_expression = cost.expr(wt, ((words, features), targets))
+    fn = theano.function(inputs=[words, features, targets],
+                         outputs=cost_expression)
+    print fn(d['train'].X1[0], d['train'].X2[0], d['train'].y[0])
+
+
 if __name__ == "__main__":
-    test()
+    #predict_test()
+    train_brown_pos()
