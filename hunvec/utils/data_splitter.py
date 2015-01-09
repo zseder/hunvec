19812a122a51e273399c44eeca65206d23465460
zseder <zseder@ilab.sztaki.hu>
2015-01-09 16:24:22 +0100

    modified:   hunvec/utils/data_splitter.py bugfix: we did shuffle while splitting, so data got mixed up

--- hunvec/utils/data_splitter.py
+++ hunvec/utils/data_splitter.py
@@ -1,13 +1,21 @@
 from random import shuffle
 
 
-def datasplit(data, ratios=[.7, .15, .15]):
-    indices = range(len(data))
+def shuffled_indices(data_len, ratios):
+    indices = range(data_len)
     shuffle(indices)
-    training = int(ratios[0] * len(data))
-    test = int(ratios[1] * len(data))
-    valid = int(ratios[2] * len(data))
+    training = int(ratios[0] * data_len)
+    test = int(ratios[1] * data_len)
+    valid = int(ratios[2] * data_len)
     training_indices = indices[:training]
     valid_indices = indices[training:training + valid]
     test_indices = indices[training+valid:training+valid+test]
-    return data[training_indices], data[test_indices], data[valid_indices]
+    return training_indices, valid_indices, test_indices
+
+
+def datasplit(data, indices, ratios=[.7, .15, .15]):
+    training_indices, valid_indices, test_indices = indices
+    tr_data = [data[i] for i in training_indices]
+    tst_data = [data[i] for i in test_indices]
+    v_data = [data[i] for i in valid_indices]
+    return tr_data, tst_data, v_data
