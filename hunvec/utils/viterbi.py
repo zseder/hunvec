f3a3b2263c4f25c7688ff8f79bfbc425cba7b5af
zseder <zseder@ilab.sztaki.hu>
2015-02-24 13:35:03 +0100

    modified:   hunvec/utils/viterbi.py our algorithms are based on logprobs, so * -> +

--- hunvec/utils/viterbi.py
+++ hunvec/utils/viterbi.py
@@ -5,7 +5,7 @@ def viterbi(start, A, end, probs, n_classes):
 
     # Initialize base cases (t == 0)
     for y in states:
-        V[0][y] = start[y] * probs[0][y]
+        V[0][y] = start[y] + probs[0][y]
         path[y] = [y]
 
     # Run Viterbi for t > 0
@@ -16,11 +16,11 @@ def viterbi(start, A, end, probs, n_classes):
         for y in states:
             if t != probs.shape[0] - 1:
                 (prob, state) = max(
-                    (V[t-1][y0] * A[y0][y] * probs[t][y], y0)
+                    (V[t-1][y0] + A[y0][y] + probs[t][y], y0)
                     for y0 in states)
             else:
                 (prob, state) = max(
-                    (V[t-1][y0] * A[y0][y] * probs[t][y] * end[y], y0)
+                    (V[t-1][y0] + A[y0][y] + probs[t][y] + end[y], y0)
                     for y0 in states)
             V[t][y] = prob
             newpath[y] = path[state] + [y]
