8a5ba3441cae3537ea2d47411d6b9446c2cc9764
Katalin Pajkossy <pajkossy@info.ilab.sztaki.hu>
2015-03-05 17:18:20 +0100

    modified:   run_exp.sh simpler

--- exp/run_exp.sh
+++ exp/run_exp.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-args=("$@")
-res_dir=`echo ${args[0]}`
-name=`echo ${args[1]}`
-hunvec_params=`echo $@|cut -f3- -d' '`
+res_dir=$1
+name=$2
+hunvec_params=`echo $@ | cut -f3- -d' '`
 python hunvec/seqtag/trainer.py ${hunvec_params} ${res_dir}/${name}.model > ${res_dir}/${name}.log
 echo ${hunvec_params} > ${res_dir}/${name}.params
