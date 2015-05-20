0a3d30e46a9a7a44d6b5cec03541686704d1e389
Pajkossy Katalin <pajkossy@bethe.phy.bme.hu>
2015-05-20 20:13:29 +0200

    modified:   raw_corpus.py

--- hunvec/corpus/raw_corpus.py
+++ hunvec/corpus/raw_corpus.py
@@ -43,21 +43,3 @@ class RawCorpus(object):
             new_wi = (self.unk if self.use_unknown else len(self.w2i))
             sen[i] = [self.w2i.setdefault(sen[i][0].lower(), new_wi)]\
                     + sen[i][1:] + [sen[i][0]]
-
-class TaggedCorpus(RawCorpus):
-
-    def __init__(self, fn, featurizer=None, w2i=None, t2i=None,
-            use_unknown=False):
-        RawCorpus.__init__(self, fn, featurizer, w2i, t2i, 
-                use_unknown)
-
-    def add_ints(self, sen):
-        RawCorpus.add_ints(self, sen)
-        print sen
-        for i in xrange(len(sen)):
-            new_ti = (self.unk if self.use_unknown else len(self.t2i))
-            sen[i][1] = self.t2i.setdefault(sen[i][1], new_ti)
-   
-    def read(self, pre=False):
-        for s in RawCorpus.read(self, pre, needed_fields=[0, 1]):
-            yield s
