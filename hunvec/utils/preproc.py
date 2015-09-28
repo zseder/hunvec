52a0c4b7da7353c4006ba939db016dde61440c16
Katalin <pajkossyk@gmail.com>
2015-09-28 18:20:26 +0200

    modified:   hunvec/utils/preproc.py 	modified:   hunvec/feature/featurizer.py

--- hunvec/utils/preproc.py
+++ hunvec/utils/preproc.py
@@ -10,13 +10,17 @@ class TrainingPreprocesser:
         self.cutoff=cutoff
         if self.num:
             self.compile_num_regex()
+        self.vocab = {}    
             
     def compile_num_regex(self):
         self.num_regex = re.compile('^[0-9\-\.]+$')
     
-    def get_vocab(self, training_fh):
+    def get_vocab(self, training_f):
+        training_fh = open(training_f)
         counts = self.get_word_count(training_fh)
-        return set(filter(lambda x:counts[x] > self.cutoff, counts.keys()))
+        training_fh.close()
+        self.vocab = set(filter(lambda x:counts[x] > 
+            self.cutoff, counts.keys()))
 
     def get_word_count(self, training_fh):
         word_counts = defaultdict(int)
@@ -31,7 +35,7 @@ class TrainingPreprocesser:
         return word_counts    
 
 
-    def write_replaced_text(self, f, vocab):
+    def write_replaced_text(self, f):
         fh = open(f)
         fh_replaced = open('{}_num_{}.cutoff_{}'.format(
             f, self.num, self.cutoff), 'w')
@@ -43,17 +47,16 @@ class TrainingPreprocesser:
             w, t = l.split('\t')
             if self.num and self.num_regex.match(w) is not None:
                 w = '__NUM__'
-            elif w.lower() not in vocab:
+            elif  self.rare and w.lower() not in self.vocab:
                 w = '__RARE__'
             fh_replaced.write(u'{}\t{}\n'.format(w, t).encode('utf-8'))    
 
     def create_replaced_files(self, training_f, devel_f, test_f):
-        training_fh = open(training_f)
-        vocab = self.get_vocab(training_fh) 
-        training_fh.close()
-        self.write_replaced_text(training_f, vocab)
-        self.write_replaced_text(devel_f, vocab)
-        self.write_replaced_text(test_f, vocab)
+        if self.rare:
+            self.get_vocab(training_f)
+        self.write_replaced_text(training_f)
+        self.write_replaced_text(devel_f)
+        self.write_replaced_text(test_f)
         
 
 
@@ -63,7 +66,7 @@ def main():
     training_f = sys.argv[1]
     devel_f = sys.argv[2]
     test_f = sys.argv[3]
-    a = TrainingPreprocesser()
+    a = TrainingPreprocesser(rare=False)
     a.create_replaced_files(training_f, devel_f, test_f)
 
 if __name__ == '__main__':
