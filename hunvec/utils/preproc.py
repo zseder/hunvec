ce4f9a7c7bbeb0927889e3f79c3ce96a8ef3a828
Katalin <pajkossyk@gmail.com>
2015-09-29 14:46:47 +0200

    modified:   hunvec/utils/preproc.py

--- hunvec/utils/preproc.py
+++ hunvec/utils/preproc.py
@@ -4,18 +4,24 @@ import re
 
 class TrainingPreprocesser:
 
-    def __init__(self, num=True, rare=True, cutoff=1):
-        self.rare = rare
+    def __init__(self, num=True, cutoff=None, list_fn=None):
         self.num = num
-        self.cutoff=cutoff
+        self.cutoff = cutoff
         if self.num:
             self.compile_num_regex()
-        self.vocab = {}    
+        self.list_fn = list_fn
+        self.vocab = None
+   
+    def get_list_vocab(self):
+        if self.list_fn != None:
+            self.vocab = set([l.strip('\n').lower() for l in open(
+                self.list_fn)])
+            print len(self.vocab)
             
     def compile_num_regex(self):
-        self.num_regex = re.compile('^[0-9\-\.]+$')
+        self.num_regex = re.compile('(^.*?)([0-9]+)(.*?$)')
     
-    def get_vocab(self, training_f):
+    def get_cutoff_vocab(self, training_f):
         training_fh = open(training_f)
         counts = self.get_word_count(training_fh)
         training_fh.close()
@@ -37,36 +43,48 @@ class TrainingPreprocesser:
 
     def write_replaced_text(self, f):
         fh = open(f)
-        fh_replaced = open('{}_num_{}.cutoff_{}'.format(
-            f, self.num, self.cutoff), 'w')
+
+        fh_replaced = open('{}_num_{}.cutoff_{}_dict_{}'.format(
+            f, self.num, self.cutoff, self.list_fn.split('/')[-1]), 'w')
         for l in fh:
+            numeric = False
             l = l.strip('\n').decode('utf-8')
             if len(l) == 0:
                 fh_replaced.write('\n')
                 continue
             w, t = l.split('\t')
-            if self.num and self.num_regex.match(w) is not None:
-                w = '__NUM__'
-            elif  self.rare and w.lower() not in self.vocab:
-                w = '__RARE__'
+            if self.num:
+                m = self.num_regex.match(w)
+                while m is not None:
+                    numeric = True
+                    w1, num, w2 = m.groups()
+                    w = w1 + '__NUM__' + w2
+                    m = self.num_regex.match(w)    
+            if numeric == False and self.vocab != None and w.lower() not in self.vocab:
+                if w.isupper():
+                    w = 'RAREWORD'
+                elif w[0].isupper() and w[1:].islower():
+                    w = 'Rareword'
+                else:
+                     w = 'rareword'
             fh_replaced.write(u'{}\t{}\n'.format(w, t).encode('utf-8'))    
 
     def create_replaced_files(self, training_f, devel_f, test_f):
-        if self.rare:
-            self.get_vocab(training_f)
+        if self.cutoff != None:
+            self.get_cutoff_vocab(training_f)
+        elif self.list_fn != None:
+            self.get_list_vocab()
         self.write_replaced_text(training_f)
         self.write_replaced_text(devel_f)
         self.write_replaced_text(test_f)
         
-
-
 
 def main():
     
     training_f = sys.argv[1]
     devel_f = sys.argv[2]
     test_f = sys.argv[3]
-    a = TrainingPreprocesser(rare=False)
+    a = TrainingPreprocesser(list_fn='/home/pajkossy/Proj/hunvec/embedding/wsj_150e_left_important')
     a.create_replaced_files(training_f, devel_f, test_f)
 
 if __name__ == '__main__':
