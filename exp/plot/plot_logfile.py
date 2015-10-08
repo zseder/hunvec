b3e62f6aa8138359a232bdc417ad2c4ee2c3306c
Katalin <pajkossy@gmail.com>
2015-10-08 18:17:42 +0200

    modified:   plot_logfile.py

--- exp/plot/plot_logfile.py
+++ exp/plot/plot_logfile.py
@@ -1,24 +1,27 @@
 import sys
-from matplotlib import plot as plt
-
-
-
+from matplotlib import pyplot as plt
+from numpy import array
+ 
+ 
+def proc(to_plot_fns, toplot_score, toplot_dataset):
+     return [[1,2,3],[1,2,3]],[[95, 96, 97],[40, 41, 42]]
+  
 def proc_and_plot(to_plot_fns, toplot_score, toplot_dataset):
-    return [1,2,3], [95, 96, 97], 'ro', [1,2,3], [96, 97, 98], 'bo'
-
-def proc_and_plot(to_plot_fns, toplot_score, toplot_dataset):
-    colours = ['ro', 'bo', 'go', 'yo']
-    plt(proc(to_plot_fns, toplot_score, toplot_dataset))
-
-
-
-
+      values = []
+      values = proc(to_plot_fns, toplot_score, toplot_dataset)
+      plt.plot(array(values[0]).T, array(values[1]).T)
+      plt.show()
+  
+  
+  
+  
 def main():
-
-    toplot_score = ','.split(sys.argv[1])
-    toplot_dataset = ','.split(sys.argv[2])
-    to_plot_fns = sys.argv[3:]
-    proc_and_plot(to_plot_fns, toplot_score, toplot_dataset)
-
+  
+      toplot_score = ','.split(sys.argv[1])
+      toplot_dataset = ','.split(sys.argv[2])
+      to_plot_fns = sys.argv[3:]
+      proc_and_plot(to_plot_fns, toplot_score, toplot_dataset)
+  
 if __name__ == "__main__":
-    main()
+      main()
+
