cfb9fd4539ec235d2e5264456486a969e1bd10e5
Katalin <pajkossyk@gmail.com>
2015-10-19 18:26:01 +0200

    m

--- exp/run_exp.sh
+++ exp/run_exp.sh
@@ -6,7 +6,5 @@ if [ -f ${res_dir}/${name}.model ];
 then 
 	echo "file ${res_dir}/${name}.model exists"
 fi
-date +"%m/%d/%Y %H:%M:%S" > ${res_dir}/${name}.log
-nice python hunvec/seqtag/trainer.py ${hunvec_params} ${res_dir}/${name}.model >> ${res_dir}/${name}.log 2> ${res_dir}/${name}.err  
-date +"%m/%d/%Y %H:%M:%S" >> ${res_dir}/${name}.log
+nice python hunvec/seqtag/trainer.py ${hunvec_params} ${res_dir}/${name}.model > ${res_dir}/${name}.log 2> ${res_dir}/${name}.err &
 echo ${hunvec_params} > ${res_dir}/${name}.params
